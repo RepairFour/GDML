fca259c637f3b246f4ce557431bfe599c7cfa0b3
WORKSTATION\Cameron <cameronmurphyofficial@gmail.com>
2021-03-30 12:17:52 +1100

    Started making a boss state machine
    Begun build a state machine for the tutorial boss

---
+++ Prototypes/Abyssal Unchained/Assets/Scripts/BossStateMachine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BossState
+{
+    Idle,
+    WideUp1,
+    WideUp2,
+    WideUp3,
+    WideUp4,
+    Attack1,
+    Attack2,
+    Attack3,
+    Attack4,
+}
+
+public class BossStateMachine : MonoBehaviour
+{
+    BossState currentState;
+    void Start()
+    {
+        currentState = BossState.Idle;
+    }
+
+    public void ChangeState(BossState bs)
+	{
+        currentState = bs;
+        RunState();
+    }
+
+    void RunState()
+	{
+
+	}
+
+    public BossState CurrentState()
+    {
+        return currentState;
+    }
+}
