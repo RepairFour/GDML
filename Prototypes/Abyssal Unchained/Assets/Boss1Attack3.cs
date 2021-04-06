1c3070f0872261b6ac3ecc0dc742c565b2d74820
psychedelic_Fish <joshua.ferguson91@gmail.com>
2021-04-06 15:40:20 +0930

    Fixed collider issue on laser beam

--- Prototypes/Abyssal Unchained/Assets/Boss1Attack3.cs
+++ Prototypes/Abyssal Unchained/Assets/Boss1Attack3.cs
@@ -9,7 +9,9 @@ public class Boss1Attack3 : MonoBehaviour
 	{
 		if(collision.GetComponent<Player>() != null)
 		{
-			Vector2 direction = FindObjectOfType<CharacterController2D>().transform.position - FindObjectOfType<TestBoss>().transform.position;
+            //Debug.DrawLine(FindObjectOfType<CharacterController2D>().transform.position, FindObjectOfType<TestBoss>().transform.position);
+            //Vector2 direction = FindObjectOfType<CharacterController2D>().transform.position - FindObjectOfType<TestBoss>().transform.position;
+            Vector2 direction = collision.transform.position - GetComponentInParent<TestBoss>().transform.position;
 			RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position,direction, 10000, mask);
 			if(hit)
 			{
