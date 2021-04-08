0c1c937f43266266ac1b14b8b57b2d7b89cb65be
WORKSTATION\Cameron <cameronmurphyofficial@gmail.com>
2021-04-08 12:38:32 +1000

    Created tutorial screen

---
+++ Prototypes/Abyssal Unchained/Assets/tutorial.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorial : MonoBehaviour
+{
+    bool showing = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(showing)
+		{
+            if(Input.anyKey)
+			{
+                showing = false;
+                gameObject.SetActive(false);
+                Time.timeScale = 1;
+			}
+		}
+    }
+}
