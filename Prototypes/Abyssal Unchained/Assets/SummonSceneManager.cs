f9988d304a61c617978449d0eb1680bfac3b808c
WORKSTATION\Cameron <cameronmurphyofficial@gmail.com>
2021-06-01 15:26:36 +1000

    Setup moving between menus
    You can now move from menu to summon, to game

---
+++ Prototypes/Abyssal Unchained/Assets/SummonSceneManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SummonSceneManager : MonoBehaviour
+{
+    public void GoBack()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+	}
+	public void SummonBoss()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
