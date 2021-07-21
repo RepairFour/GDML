6606928ed53f4a36866d02d3bdc0a5d2a671da1c
psychedelic_Fish <joshua.ferguson91@gmail.com>
2021-07-21 10:24:33 +0930

    created character controller prototype

---
+++ Prototypes/Onigatana/Assets/CameraController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+
+    public InputActionMap cameraControls;
+   
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationX = 0F;
+    float rotationY = 0F;
+
+    private List<float> rotArrayX = new List<float>();
+    float rotAverageX = 0F;
+
+    private List<float> rotArrayY = new List<float>();
+    float rotAverageY = 0F;
+
+    public float frameCounter = 20;
+
+    Quaternion originalRotation;
+
+   
+
+    void Update()
+    {
+        
+        
+        rotAverageY = 0f;
+        rotAverageX = 0f;
+
+        rotationY += cameraControls.FindAction("TrackCamera").ReadValue<Vector2>().y;
+        rotationX += cameraControls.FindAction("TrackCamera").ReadValue<Vector2>().x;
+        //rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        //rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+
+        rotArrayY.Add(rotationY);
+        rotArrayX.Add(rotationX);
+
+        if (rotArrayY.Count >= frameCounter)
+        {
+            rotArrayY.RemoveAt(0);
+        }
+        if (rotArrayX.Count >= frameCounter)
+        {
+            rotArrayX.RemoveAt(0);
+        }
+
+        for (int j = 0; j < rotArrayY.Count; j++)
+        {
+            rotAverageY += rotArrayY[j];
+        }
+        for (int i = 0; i < rotArrayX.Count; i++)
+        {
+            rotAverageX += rotArrayX[i];
+        }
+
+        rotAverageY /= rotArrayY.Count;
+        rotAverageX /= rotArrayX.Count;
+
+        rotAverageY = ClampAngle(rotAverageY, minimumY, maximumY);
+        rotAverageX = ClampAngle(rotAverageX, minimumX, maximumX);
+
+        Quaternion yQuaternion = Quaternion.AngleAxis(rotAverageY, Vector3.left);
+        Quaternion xQuaternion = Quaternion.AngleAxis(rotAverageX, Vector3.up);
+
+        transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+
+        if (cameraControls.FindAction("ShowCursor").triggered)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        
+        
+    }
+
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb)
+            rb.freezeRotation = true;
+        originalRotation = transform.localRotation;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        cameraControls.Enable();
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        angle = angle % 360;
+        if ((angle >= -360F) && (angle <= 360F))
+        {
+            if (angle < -360F)
+            {
+                angle += 360F;
+            }
+            if (angle > 360F)
+            {
+                angle -= 360F;
+            }
+        }
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ Prototypes/Onigatana/Assets/GroundedHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedHandler : MonoBehaviour
+{
+    [SerializeField]bool grounded;
+
+    public bool isGrounded { get => grounded; set => grounded = value; }
+
+    private void Start()
+    {
+        grounded = true;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+    
+}
