dd1338ad545953d6897e075347668370709daba3
WORKSTATION\Cameron <cameronmurphyofficial@gmail.com>
2021-07-21 10:58:37 +1000

    Creating health pickup and player stat scripts

---
+++ Prototypes/Onigatana/Assets/CameronsScripts/PlayerStats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+	[SerializeField] int maxHealth;
+	public int health { get; private set; }
+	public int armour { get; private set; }
+
+	private void Start()
+	{
+		health = maxHealth;
+	}
+	public bool isDead()
+	{
+        if(health <= 0)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public void ModHealth(int amount)
+	{
+		health += amount;
+	}
+	public void ModArmour(int amount)
+	{
+		armour += amount;
+	}
+}
