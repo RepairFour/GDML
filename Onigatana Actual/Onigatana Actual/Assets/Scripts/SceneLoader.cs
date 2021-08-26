84266a0f8932ee01c2fa00198ab9c75662f2c8ef
psychedelic_Fish <joshua.ferguson91@gmail.com>
2021-08-26 11:41:18 +0930

    Updfated the menu to contain a discalimer, set upo the buttons

---
+++ Onigatana Actual/Onigatana Actual/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayClicked()
+    {
+        SceneManager.LoadScene("1");
+    }
+    public void QuitClicked()
+    {
+        Application.Quit();
+    }
+}
