2a3087699df907926d8305eca9464fbc7a6a4a77
SpooksandNull <zanekerin@gmail.com>
2021-09-22 19:30:44 +1000

    updated greybox, created new scene for gun testing, added a bunch of packages, removed old Abyssal in Chains and Old Onigatana Files

--- Onigatana Actual/Onigatana Actual/Assets/Input/PlayerMap.cs
+++ Onigatana Actual/Onigatana Actual/Assets/Input/PlayerMap.cs
@@ -81,6 +81,22 @@ public class @PlayerMap : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Vector2"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Shoot"",
+                    ""type"": ""Button"",
+                    ""id"": ""1567007d-dd71-494e-9b6c-03d2ab072d7e"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Reload"",
+                    ""type"": ""Button"",
+                    ""id"": ""45190eb4-f46d-4254-b495-78cd06c16832"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -215,6 +231,28 @@ public class @PlayerMap : IInputActionCollection, IDisposable
                     ""action"": ""MousePosition"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""95849a1c-41d3-45a8-92aa-4b0377ce7b8e"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Shoot"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""20cffd04-8d8a-4104-8136-f4e759a6ee98"",
+                    ""path"": ""<Keyboard>/r"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Reload"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -231,6 +269,8 @@ public class @PlayerMap : IInputActionCollection, IDisposable
         m_Player_Hook = m_Player.FindAction("Hook", throwIfNotFound: true);
         m_Player_Attack = m_Player.FindAction("Attack", throwIfNotFound: true);
         m_Player_MousePosition = m_Player.FindAction("MousePosition", throwIfNotFound: true);
+        m_Player_Shoot = m_Player.FindAction("Shoot", throwIfNotFound: true);
+        m_Player_Reload = m_Player.FindAction("Reload", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -288,6 +328,8 @@ public class @PlayerMap : IInputActionCollection, IDisposable
     private readonly InputAction m_Player_Hook;
     private readonly InputAction m_Player_Attack;
     private readonly InputAction m_Player_MousePosition;
+    private readonly InputAction m_Player_Shoot;
+    private readonly InputAction m_Player_Reload;
     public struct PlayerActions
     {
         private @PlayerMap m_Wrapper;
@@ -300,6 +342,8 @@ public class @PlayerMap : IInputActionCollection, IDisposable
         public InputAction @Hook => m_Wrapper.m_Player_Hook;
         public InputAction @Attack => m_Wrapper.m_Player_Attack;
         public InputAction @MousePosition => m_Wrapper.m_Player_MousePosition;
+        public InputAction @Shoot => m_Wrapper.m_Player_Shoot;
+        public InputAction @Reload => m_Wrapper.m_Player_Reload;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -333,6 +377,12 @@ public class @PlayerMap : IInputActionCollection, IDisposable
                 @MousePosition.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMousePosition;
                 @MousePosition.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMousePosition;
                 @MousePosition.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMousePosition;
+                @Shoot.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
+                @Shoot.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
+                @Shoot.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnShoot;
+                @Reload.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnReload;
+                @Reload.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnReload;
+                @Reload.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnReload;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
@@ -361,6 +411,12 @@ public class @PlayerMap : IInputActionCollection, IDisposable
                 @MousePosition.started += instance.OnMousePosition;
                 @MousePosition.performed += instance.OnMousePosition;
                 @MousePosition.canceled += instance.OnMousePosition;
+                @Shoot.started += instance.OnShoot;
+                @Shoot.performed += instance.OnShoot;
+                @Shoot.canceled += instance.OnShoot;
+                @Reload.started += instance.OnReload;
+                @Reload.performed += instance.OnReload;
+                @Reload.canceled += instance.OnReload;
             }
         }
     }
@@ -375,5 +431,7 @@ public class @PlayerMap : IInputActionCollection, IDisposable
         void OnHook(InputAction.CallbackContext context);
         void OnAttack(InputAction.CallbackContext context);
         void OnMousePosition(InputAction.CallbackContext context);
+        void OnShoot(InputAction.CallbackContext context);
+        void OnReload(InputAction.CallbackContext context);
     }
 }
---
+++ Onigatana Actual/Onigatana Actual/Assets/Scenes/ZaneTestScene/GunTest.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem.Controls;
+
+
+public class GunTest : MonoBehaviour
+{
+
+    PlayerMap input;
+
+    public GameObject bullet;
+    public float shootForce, upwardForce;
+    public float timeBetweenShooting, spread, reloadTime, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+
+    int bulletsLeft, bulletsShot;
+    bool shooting, readyToShoot, reloading;
+
+    public Camera fpsCam;
+    public Transform attackPoint;
+
+    public GameObject muzzleFlash;
+    public TextMeshProUGUI ammunitionDisplay;
+
+    public AudioClip gunShotSFX;
+    public AudioClip reloadSFX;
+
+
+
+    public bool allowInvoke = true;
+
+
+    private void Awake()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+
+        input = new PlayerMap();
+        input.Enable();
+    }
+
+    private void Update()
+    {
+        MyInput();
+
+        if (ammunitionDisplay != null)
+            ammunitionDisplay.SetText(bulletsLeft / bulletsPerTap + " / " + magazineSize / bulletsPerTap);
+    }
+
+    private void MyInput()
+    {
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = input.Player.Shoot.triggered;
+
+        if (input.Player.Reload.triggered && bulletsLeft < magazineSize && !reloading) 
+            Reload();
+
+        if (readyToShoot && shooting && !reloading && bulletsLeft <= 0) 
+            Reload();
+
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            bulletsShot = 0;
+
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("bulletFired");
+        readyToShoot = false;
+
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else
+            targetPoint = ray.GetPoint(75);
+
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+        float x = Random.Range(-spread, spread);
+        Debug.Log("X Spread Value = " + x);
+        float y = Random.Range(-spread, spread);
+        Debug.Log("T Spread Value = " + y);
+        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0);
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+        currentBullet.transform.forward = directionWithSpread.normalized;
+
+
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
+        currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
+
+        if (muzzleFlash != null)
+        {
+            var newMuzzleFlash = Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+            newMuzzleFlash.transform.parent = gameObject.transform;
+        }
+        gameObject.GetComponent<AudioSource>().PlayOneShot(gunShotSFX, 0.1f);
+
+
+        bulletsLeft--;
+        bulletsShot++;
+
+
+
+        if (allowInvoke)
+        {
+            Invoke("ResetShot", timeBetweenShooting);
+            allowInvoke = false;
+        }
+
+        if (bulletsShot < bulletsPerTap && bulletsLeft > 0)
+            Invoke("Shoot", timeBetweenShots);
+
+      
+
+    }
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowInvoke = true;
+        //this is only for the shotgun's functionality
+        if (bulletsLeft <= 0)
+        {
+            Reload();
+        }
+
+
+    }
+
+    private void Reload()
+    {
+        reloading = true;
+        gameObject.GetComponent<AudioSource>().PlayOneShot(reloadSFX, 2);
+        Invoke("ReloadFinished", reloadTime);
+
+    }
+    private void ReloadFinished()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+
+    }
+}
