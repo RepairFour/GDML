eb5495e36a32595b58d346113609790e1b9d976c
SpooksandNull <zanekerin@gmail.com>
2021-10-07 13:33:37 +1100

    Updated Preferences

--- Onigatana Actual/Onigatana Actual/Assets/Scripts/Billboard.cs
+++ Onigatana Actual/Onigatana Actual/Assets/Scripts/Billboard.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-    public class Billboard : MonoBehaviour
+public class Billboard : MonoBehaviour
+{
+    void Update()
     {
-        void Update()
-        {
         transform.forward = new Vector3(Camera.main.transform.forward.x, transform.forward.y, Camera.main.transform.forward.z);
     }
-    }
+
+}
