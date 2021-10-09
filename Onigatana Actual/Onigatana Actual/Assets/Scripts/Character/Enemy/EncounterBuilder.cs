361b5c4cd2ce663847f79012bff989f7de395daa
WORKSTATION\Cameron <cameronmurphyofficial@gmail.com>
2021-10-09 17:08:52 +1100

    Fixed Timed waves to not insatantly spawn the key. Option to move onto the next wave on timed encounters. Added wipe flexibility

--- Onigatana Actual/Onigatana Actual/Assets/Scripts/Character/Enemy/EncounterBuilder.cs
+++ Onigatana Actual/Onigatana Actual/Assets/Scripts/Character/Enemy/EncounterBuilder.cs
@@ -20,12 +20,16 @@ public class EncounterBuilder : MonoBehaviour
 	[SerializeField] List<Wave> encounter = new List<Wave>();
 	[SerializeField] bool spawnNextOnWipe;
 	[SerializeField] float spawnTimerMax;
+	[SerializeField] bool spawnTimedEnemiesOnWipe;
+	[SerializeField] int wipeFlexibility;
 	[SerializeField] GameObject keyDrop;
+
 
 	float spawnTimer = 0;
 	BoxCollider zone;
 	int waveTracker = 0;
 	List<GameObject> enemiesInWave = new List<GameObject>();
+	List<GameObject> enemiesSpawned = new List<GameObject>();
 
 	private void Start()
 	{
@@ -37,35 +41,47 @@ public class EncounterBuilder : MonoBehaviour
     {
 		if(spawnNextOnWipe)
 		{
-			foreach(var enemy in enemiesInWave)
+			if(!IsWaveAlive())
 			{
-				if(enemy != null)
-				{
-					return;
-				}
-			}
-			SpawnNextWave();
+				SpawnNextWave();
+			}			
 		}
 		else
 		{
 			spawnTimer += Time.deltaTime;
-			if(spawnTimer >= spawnTimerMax)
+			if (spawnTimedEnemiesOnWipe)
 			{
-				SpawnNextWave();
-				spawnTimer = 0;
+				if (spawnTimer >= spawnTimerMax || !IsAnyWaveAlive())
+				{
+					SpawnNextWave();
+					spawnTimer = 0;
+				}
+			}
+			else
+			{
+				if (spawnTimer >= spawnTimerMax)
+				{
+					SpawnNextWave();
+					spawnTimer = 0;
+				}
 			}
 		}
-    }
+		if (waveTracker == encounter.Count && !IsAnyWaveAlive())
+		{
+			if (keyDrop != null)
+			{
+				keyDrop.SetActive(true);
+			}
+			gameObject.SetActive(false);
+			return;
+		}
+
+	}
 
 	void SpawnNextWave()
 	{
 		if(waveTracker == encounter.Count)
 		{
-			if(keyDrop != null)
-			{
-				keyDrop.SetActive(true);
-			}
-			gameObject.SetActive(false);
 			return;
 		}
 		enemiesInWave.Clear();
@@ -77,9 +93,45 @@ public class EncounterBuilder : MonoBehaviour
 				spawnPos.x += Random.Range(-zone.bounds.extents.x, zone.bounds.extents.x);
 				spawnPos.y += Random.Range(-zone.bounds.extents.y, zone.bounds.extents.y);
 				spawnPos.z += Random.Range(-zone.bounds.extents.z, zone.bounds.extents.z);
-				enemiesInWave.Add(Instantiate(enemyType.enemyPrefab, spawnPos, Quaternion.identity));
+				GameObject enemy = Instantiate(enemyType.enemyPrefab, spawnPos, Quaternion.identity);
+				enemiesInWave.Add(enemy);
+				enemiesSpawned.Add(enemy);
 			}
 		}
 		++waveTracker;
+	}
+
+	bool IsWaveAlive()
+	{
+		int numberAlive = 0;
+		foreach (var enemy in enemiesInWave)
+		{
+			if (enemy != null)
+			{
+				++numberAlive;				
+			}
+		}
+		if(numberAlive > wipeFlexibility)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	bool IsAnyWaveAlive()
+	{
+		int numberAlive = 0;
+		foreach (var enemy in enemiesSpawned)
+		{
+			if (enemy != null)
+			{
+				++numberAlive;
+			}
+		}
+		if (numberAlive > wipeFlexibility)
+		{
+			return true;
+		}
+		return false;
 	}
 }
