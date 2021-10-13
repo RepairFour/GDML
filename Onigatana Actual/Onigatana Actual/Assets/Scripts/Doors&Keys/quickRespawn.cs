054a637a314b36cb4e4df9ddab1171c5b1368950
WORKSTATION\Cameron <cameronmurphyofficial@gmail.com>
2021-10-13 11:03:52 +1100

    Fixed respawn issue

--- Onigatana Actual/Onigatana Actual/Assets/Scripts/Doors&Keys/quickRespawn.cs
+++ Onigatana Actual/Onigatana Actual/Assets/Scripts/Doors&Keys/quickRespawn.cs
@@ -2,40 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
 
 public class quickRespawn : MonoBehaviour
 {
-    public GameObject Player;
-    Controller controller;
-    public GameObject PlayerPrefab;
-    int deathNumber;
-    bool sendAnalytics;
+    //public GameObject Player;
+    //Controller controller;
+    //public GameObject PlayerPrefab;
+    //int deathNumber;
+    //bool sendAnalytics;
 
 
-    public Vector3 respawnLocation;
+    //public Vector3 respawnLocation;
 
 
-    private void Start()
-    {
-        deathNumber = 0;
-        controller = Player.GetComponent<Controller>();
-    }
+    //private void Start()
+    //{
+    //    deathNumber = 0;
+    //    controller = Player.GetComponent<Controller>();
+    //}
     private void OnTriggerStay(Collider other)
     {
-        Debug.Log("triggered 11");
-        if (other.gameObject.tag == "Player")
-        {
-            deathNumber++;
-            Debug.Log("triggered 14");
+        //Debug.Log("triggered 11");
+        //if (other.gameObject.tag == "Player")
+        //{
+        //    deathNumber++;
+        //    Debug.Log("triggered 14");
 
-            //controller.SendAnalytics(deathNumber);
-      
-            Destroy(Player);
-            Player = Instantiate(PlayerPrefab, respawnLocation, Quaternion.identity); //respawn
+        //    //controller.SendAnalytics(deathNumber);
+
+        //    Destroy(Player);
+        //    Player = Instantiate(PlayerPrefab, respawnLocation, Quaternion.identity); //respawn
 
 
-            controller = Player.GetComponent<Controller>();
-            
-        }
+        //    controller = Player.GetComponent<Controller>();
+
+        //}
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
